45eaca44697ee2c94d6b0cb1f59e861a6ee36c49
muazrana <muazrana@gmail.com>
2019-09-27 11:47:57 +0100

    My first Flask App

---
+++ Flask_App/application/models.py
@@ -0,0 +1,36 @@
+from application import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(id):
+	return Users.query.get(int(id))
+
+class Posts(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(30), nullable=False)
+	last_name = db.Column(db.String(30), nullable=False)
+	title = db.Column(db.String(100), nullable=False)
+	content = db.Column(db.String(10000), nullable=False, unique=True)
+	date_posted = db.Column(db.datetime, nullable=False, default=datetime.utcnow)
+
+	user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+	def __repr__(self):
+		return ''.join( [
+			'User: ', self.first_name, ' ', self.last_name, '\r\n',
+			'Title: ', self.title, '\r\n', self.content
+		])
+
+class Users(db.Model, UserMixin):
+	id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.string(30), nullable=False)
+	last_name = db.Column(db.string(30), nullable=False)
+	email = db.Column(db.String(150), nullable=False, unique=True)
+	password = db.Column(db.String(50), nullable=False)
+	posts = db.relationship('Posts', backref='author', lazy=True)
+
+	def __repr__(self):
+		return''.join(['User ID: ', str(self.id), '\r\n', 
+			'Email: ', self.email, '\r\n',
+			'Name: ', self.first_name, ' ', self.last_name])
---
+++ Flask_App/application/routes.py
@@ -0,0 +1,83 @@
+from flask import render_template, redirect, url_for, request
+from application import app, db, bcrypt
+from application.models import Posts, Users
+from application.forms import PostForm, RegistrationForm, LoginForm, UpdateAccountForm
+from flask_login import current_user, login_user, logout_user, login_required
+
+@app.route('/')
+@app.route('/home')
+def home():
+	postData = Posts.query.all()
+	return render_template('home.html', title='Home', posts=postData)
+
+@app.route('/about')
+def about():
+	return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		hashed_pw = bcrypt.generate_password_hash(form.password.data)
+		user = Users(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data, password=hashed_pw)
+		db.session.add(user)
+		db.session.commit()
+		return redirect(url_for('post'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/post', methods=['GET', 'POST'])
+@login_required
+def post():
+	form = PostForm()
+	if form.validate_on_submit():
+		postData = Posts(
+			first_name=form.first_name.data,
+			last_name=form.last_name.data,
+			title=form.title.data,
+			content=form.content.data
+			author=current_user
+		)
+		db.session.add(postData)
+		db.session.commit()
+		return redirect(url_for('home'))
+	else:
+		print(form.errors)
+	return render_template('post.html', title='Post', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	if current_user.is_authenticated:
+		return redirect(url_for('home'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email=form.email.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
+			next_page = request.args.get('next')
+			if next_page:
+				return redirect(next_page)
+			else:
+				return redirect(url_for('home'))
+	return render_template('login.html', title='login', form=form)
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for('login'))
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+	form = UpdateAccountForm()
+	if form.validate_on_submit:
+		current_user.first_name = form.first_name.data
+		current_user.last_name = form.last_name.data
+		current_user.email = form.email.data
+	elif request.method == 'GET':
+		form.email.data = current_user.email
+		form.first_name.data = current_user.first_name
+		form.last_name.data = current_user.last_name
+	return render_template('account.html', title='Account', form=form)
+
+	
